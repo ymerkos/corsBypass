45bdbb88aec1994045589d7c981af118377cfac4
ymerkos <121984148+ymerkos@users.noreply.github.com>
2024-12-05 01:40:27 -0500

    B"H

---
+++ index.js
@@ -0,0 +1,14 @@
+//B"H
+
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello, World!\n');
+});
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+    console.log(`Server running at http://localhost:${PORT}/`);
+});
